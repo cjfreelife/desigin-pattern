18dfe92196177cc57ce562b8dd3729e4395d7cdd
cjfreelife <517870668@qq.com>
2019-03-17 16:38:54 +0800

    delegate

--- delegate-pattern/src/main/java/org/cj/delegate/DelegateTest.java
+++ delegate-pattern/src/main/java/org/cj/delegate/DelegateTest.java
@@ -1,6 +1,12 @@
 package org.cj.delegate;
 
+/**
+ * 委派模式-通过一个处理类，不管过程，根据不同要求进行不同处理，只关心结果，行为型。
+ * 主要
+ */
 public class DelegateTest {
+
+
     public static void main(String[] args) {
         Leader leader = new Leader();
         leader.doWork("a");
